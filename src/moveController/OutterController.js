04b80f0a23ae549a905a5e24c49ffc9f633f6ec4
woodpeckerAnos <woodpeckeranos@foxmail.com>
2020-05-25 17:53:54 +0800

    update outerController

--- src/moveController/OutterController.js
+++ src/moveController/OutterController.js
@@ -51,9 +51,25 @@ export function OutterController(props)  {
 
     useEffect(() => {
         // 运动行为发生了变化
-        moveStateReducer(moveFlag, route, changeMovingHandler)
+        changeMovingHandler(moveStateReducer(moveFlag, route, animationStep, updateAnimationStep))
     }, [moveFlag])
 
+    // 获取运动快照
+    function getSnapShot({timeStamp, snapShot}) {
+        const {timeStamp : timeStampArr, moveSnapShot : prevSnapShot} = animationStep
+        updateAnimationStep({
+            timeStamp: [...timeStampArr, timeStamp],
+            snapShot: {
+                ...prevSnapShot,
+                [timeStamp]: snapShot
+            }
+        })
+    }
+
+    props = {
+        ...props,
+        getSnapShot,
+    }
     return (
         <div> 
             {renderMoveUnit(shouldRendering, props)}
@@ -71,36 +87,71 @@ function renderMoveUnit(renderFlag, props) {
 // 处理状态变更
 // —— 传递给运动组件一个运算函数，运动组件内部根据这个运算函数来处理自身的变量
 // —— 类似于传递了一个自定义hook，内部运动组件根据这个hook的变化就可以判断是否进行新一次的运动
-function moveStateReducer(moveState, route, changeMovingReducer) {
+// 该状态变更只处理同一route运动中的运动状态变化，不承担更新route的机能
+function moveStateReducer(moveState, route, animationStep, updateAnimationStep) {
     let newHandler = function (input) { return input }
+    let step = 0
+
+    // 根据animationSteps数据处理移动route的方法
+    const { timeStamp, moveSnapShot } = animationStep
+    const latestTimeStamp = timeStamp.slice(-1)[0]
+    // 根据时间戳预估位置
+    // caution: 当运动为非线性的时候，本质是根据速率函数来控制每个点之间的间距，因此帧数和索引应该不会有什么变化
+    const probablyRange = [Math.floor(latestTimeStamp/1000*60 - 10), Math.ceil(latestTimeStamp/1000*60 + 10)] // 60:frames  10:adjustNum
+    const currentIndex = probablyRange[0] + route.slice(probablyRange[0], probablyRange[1] + 1)
+        .findIndex(item => item.x === moveSnapShot[latestTimeStamp].x && item.y === moveSnapShot[latestTimeStamp].y)
+    route = route.slice(currentIndex) // 生成最新的route
+
     if (moveState === '1') {
         // 调用运行的方法
-        
+        newHandler = () => {
+            step++
+            if (step >= route.length) {
+                return {
+                    x: route[route.length - 1].x,
+                    y: route[route.length - 1].y,
+                }
+            }
+            return {
+                x: route[step].x,
+                y: route[step].y,
+            }
+        }
     }
     if (moveState === '2') {
         // 调用停止的方法 
+        // 停止方法会保持结果值和输入值一样
+        // 内部运动组件根据4点原则（0% 25% 50% 100%）确定位置没有发生变化，则维护自身为停止
+        newHandler = (input) => {
+            return {
+                x: input.x,
+                y: input.y,
+            }
+        }
         // 调用快照记录的方法
+        // 记录快照的方法由父组件统一维护，传递给运动组件进行调用
+
     }
     if (moveState === '3') {
-        // 调用获取指定快照数据的方法
+        // 从逻辑一致性上来说，先要获取当前快照信息
+        // 如果没有快照信息则更新添加进本次快照信息
+        // 如果有这个快照信息，则不进行快照更新处理
 
-        // 设定运行目标的方法
+        // 记录运动信息后，反转route数组，使用反转的route数组路劲数据既可以反向运动
 
-        // 调用运行的方法
+        // 生成运行的handler
     }
     if (moveState === '4') {
-        // 调用运行的方法
-
-        // 调用立即完成的方法
+        // 生成直接抵达当前route终点的handler
     }
     if (moveState === '5') {
         // 调用获取指定快照数据的方法
-
+  
         // 设定运行目标的方法
 
         // 调用运行的方法
     }
-    changeMovingReducer(newReducer)
+    return newReducer
 }
 
 // 改变状态
